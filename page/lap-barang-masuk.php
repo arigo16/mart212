2a51930fb032277ae44818914c5279cd05ac96be
DESKTOP-HFBGGH7\Arigo <arigo@edutren.com>
2018-08-22 10:20:04 +0700

    updated 22 agt 2018

---
+++ page/lap-barang-masuk.php
@@ -0,0 +1,57 @@
+<?php
+    $today = date("Y-m-d");
+    $today30 = date('Y-m-d', strtotime("-30 day"));
+?>
+
+<div class="content custom-scrollbar">
+
+    <div id="e-commerce-product" class="page-layout simple tabbed">
+
+        <!-- HEADER -->
+        <div class="page-header bg-secondary text-auto row no-gutters align-items-center justify-content-between p-6">
+
+            <div class="row no-gutters align-items-center">
+
+                <div class="h2">Laporan Transaksi Barang Masuk</div>
+            </div>
+
+        </div>
+        <!-- / HEADER -->
+
+        <!-- CONTENT -->
+        <div class="page-content">
+
+            <div class="tab-content">
+
+                <div class="tab-pane fade show active" id="basic-info-tab-pane" role="tabpanel" aria-labelledby="basic-info-tab">
+
+                    <div class="card p-6">
+                        <form action="report/laporan-barang-masuk-cetak.php" method="POST" target="_blank">
+                            <div class="card">
+                                <div class="card-header">
+                                    Laporan Per Periode
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-row">
+                                        <div class="col-6 form-group">
+                                            <input class="form-control" type="date" value="<?php echo $today30 ?>" name="tgl_mulai"/>
+                                            <label>Tanggal Mulai</label>
+                                        </div>
+                                        <div class="col-6 form-group">
+                                            <input class="form-control" type="date"  value="<?php echo $today ?>" name="tgl_akhir"/>
+                                            <label>Tanggal Akhir</label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-secondary">LIHAT</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!-- / CONTENT -->
+    </div>
+
+</div>
