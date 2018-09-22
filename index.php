7546d9020ff3c36c69f487fa4e866d3a41747662
DESKTOP-HFBGGH7\Arigo <arigo@edutren.com>
2018-09-22 16:39:51 +0700

    baru

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <html lang="en-us">
 
 <head>
-    <title>Menu Utamas</title>
+    <title>Menu Utama</title>
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
